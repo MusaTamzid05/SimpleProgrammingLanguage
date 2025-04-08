590356144ad97f4cfce41376152d21587cb149c2
MusaTamzid05 <tamzid101@gmail.com>
2025-04-09 02:52:15 +0600

    Add repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include "lexer.h"
 #include "lexer_test.h"
 
 void run_tests();
+void run_repl();
 
 int main(int argc, char** argv) {
-    run_tests();
+    run_repl();
     return 0;
 }
 
@@ -23,6 +25,38 @@ void run_tests() {
         }
 
         delete test;
+
+    }
+
+}
+
+void run_repl() {
+    bool running = true;
+    std::string input;
+
+    while(running) {
+        std::cout << ">>> ";
+        std::getline(std::cin, input);
+
+        if(input == "exit") {
+            running = false;
+            continue;
+        }
+
+        Lexer lexer(input);
+
+        bool file_end_found = false;
+
+        while(file_end_found == false) {
+            Token token = lexer.next_token();
+
+            std::cout << "type " << token.type << " literal " << token.literal << "\n";
+
+            if(token.type == token_type::EOF_TYPE) 
+                file_end_found = true;
+
+        }
+        
 
     }
 
